f404fa70499e836aed275d5ca6e380fb335d5ba1
shubhamantino20 <Shubham.k@antino.io>
2023-03-14 17:19:07 +0530

    ss

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post("/create-checkout-session", async (req, res) => {
     })
     res.json({ url: session.url })
   } catch (e) {
-    // res.status(500).json({ error: e.message })
+    res.status(500).json({ error: e.message })
   }
 })
 
